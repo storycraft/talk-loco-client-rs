b612cea4402b9288bade646e02516c0185371318
storycraft <storycraft@pancake.sh>
2021-12-10 15:48:45 +0900

    Add checkin example

--- examples/checkin.rs
+++ examples/checkin.rs
@@ -11,9 +11,9 @@ use loco_protocol::secure::{
 };
 use rsa::{pkcs8::FromPublicKey, RsaPublicKey};
 use talk_loco_client::{
-    command::{manager::BsonCommandManager, session::BsonCommandSession, BsonCommand},
+    client::checkin::CheckinClient,
+    command::{manager::BsonCommandManager, session::BsonCommandSession},
     request,
-    response::{self, ResponseData},
     stream::ChunkedWriteStream,
     structs::client::ClientInfo,
 };
@@ -44,28 +44,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loco_session.handshake_async(&mut stream).await?;
 
     let mut checkin_conn = BsonCommandSession::new(BsonCommandManager::new(stream));
+    let mut checkin_client = CheckinClient(&mut checkin_conn);
 
-    let checkin_req = checkin_conn
-        .request_async(&BsonCommand::new_const(
-            "CHECKIN",
-            0,
-            request::checkin::Checkin {
-                user_id: 1,
-                client: ClientInfo {
-                    os: "win32".into(),
-                    net_type: 0,
-                    app_version: "3.2.8".into(),
-                    mccmnc: "999".into(),
-                },
-                language: "ko".into(),
-                country_iso: "KR".into(),
-                use_usb: true,
+    let checkin_res = checkin_client
+        .checkin(&request::checkin::Checkin {
+            user_id: 1,
+            client: ClientInfo {
+                os: "win32".into(),
+                net_type: 0,
+                app_version: "3.2.8".into(),
+                mccmnc: "999".into(),
             },
-        ))
+            language: "ko".into(),
+            country_iso: "KR".into(),
+            use_usb: true,
+        })
         .await?;
-
-    let checkin_res = checkin_conn.response_async(checkin_req).await?;
-    let checkin_res = checkin_res.try_deserialize::<ResponseData<response::checkin::Checkin>>().unwrap();
 
     println!("CHECKIN response: {:?}", checkin_res);
 
